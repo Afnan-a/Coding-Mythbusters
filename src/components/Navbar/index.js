cf9f8fa8edf074de97ba92d54a495ce8d3d479f5
ajalley18288 <91692414+ajalley18288@users.noreply.github.com>
2021-10-07 12:00:42 -0500

    Nav bar starting point

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+Nav,
+NavLink,
+Bars,
+NavMenu,
+NavBtn,
+NavBtnLink,
+} from './NavbarElements';
+
+const Navbar = () => {
+return (
+	<>
+	<Nav>
+		<Bars />
+
+		<NavMenu>
+		<NavLink to='/welcome' activeStyle>
+			Welcome
+		</NavLink>
+        <NavLink to='/home' activeStyle>
+            Home
+        </NavLink>
+		</NavMenu>
+	</Nav>
+	</>
+);
+};
+
+export default Navbar;
