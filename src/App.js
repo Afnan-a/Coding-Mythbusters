7b5b56cb26c751a86cbf391c887c7e2082e70564
Sam Lucas <samlucas383@gmail.com>
2021-10-19 12:39:56 -0500

    Created Calendar & Curriulum page and added to Navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './screens/home';
 import Welcome from './screens/welcome';
 import Lessons from './screens/lessons';
+import Calendar from './screens/calendar';
+import Curriculum from './screens/curriculum';
 
 function App() {
 return (
@@ -13,7 +15,9 @@ return (
 	<Switch>
 		<Route path='/home' exact component={Home} />
 		<Route path='/welcome' component={Welcome} />
-		<Route path='/lesson' component={Lessons} />
+		<Route path='/lessons' component={Lessons} />
+		<Route path='/calendar' component={Calendar} />
+		<Route path='/curriculum' component={Curriculum} />
 	</Switch>
 	</Router>
 );
