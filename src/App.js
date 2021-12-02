3cabe03ff52d3dbc73f447a777bbf3efbd08d944
samlucas383 <77863365+samlucas383@users.noreply.github.com>
2021-12-02 12:09:53 -0600

    cleaned up commented out code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Settings from './screens/settings';
 import People from './screens/people';
 import Logout from './screens/log_out';
 import PrivateRoute from './screens/privateroute'
-import Signin from './screens/sign_in';
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from "./context/auth_context"
 
@@ -32,14 +31,7 @@ return (
 			</Switch>
 		</AuthProvider>
 	</Router>
-	/*<Container 
-		className="d-flex align-items-center justify-content-center"
-		style={{minHeight: "100vh" }}
-	>
-		<div className="w-100" style={{maxWidth: "400px" }}>
-			<Signin />
-		</div>
-	</Container>*/
+
 	
 );
 }
