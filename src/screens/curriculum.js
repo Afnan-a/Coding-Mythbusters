a8aa92e3cd2639c6856b17f3a448fc17804ffd15
Afnan-a <afnan506@gmail.com>
2021-12-04 12:26:19 -0600

    Delete and Download has been added

--- src/screens/curriculum.js
+++ src/screens/curriculum.js
@@ -1,126 +1,76 @@
 import React, { useEffect, useState } from 'react';
-import { Viewer } from '@react-pdf-viewer/core'; 
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { Worker } from '@react-pdf-viewer/core';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import storage from './firebase';
+import ListGroup from 'react-bootstrap/ListGroup'
 
- 
 const Curriculum = () => {
-  // define new handler, we need to handel event (e) and the refrence to the file
-  const [Url, setUrl] = useState('');
-  const [file, setFile] = useState('');
+ // define new handler, we need to handel event (e) and the refrence to the file
+ const [Url, setUrl] = useState('');
+ const [file, setFile] = useState('');
+ const [data, setData] = useState([]);
 
-  const upload = () => {
-    if (file ==  null) 
-      return;
-    
-    // sending file to storage
-    storage.ref(`/curriculum/${file.name}`).put(file).on("state_changed", () => {
-      storage.ref("curriculum").child(file.name).getDownloadURL().then((Url) => {
-        setUrl(Url);
-      })
-    });
-
-  }
-
-  /*
-  const onChange = (e) => {
-    const file = e.target.files[0];  // file refrence
-    storage.ref( `/curriculum/${file.name}`).put(file).then (() => {
-      console.log("Uploaded a file", file.name);
-      storage.ref("curriculum").child(file.name).getDownloadURL().then ((url) => {setUrl(url);})
-    })
-  };*/
-
-  return(
-   // <input type="file" onChange={onChange}/> // onchange handler for this input
-   <div className="Curriculum">
-    <center>
-      <input type="file" onChange={(e) => { setFile(e.target.files[0]) }} />
-      <button onClick={upload}>Upload</button>
-      <p><a href={Url}>{Url}</a></p>
-      <br />
-    </center>
-   </div>
-   
-  );
-  /*
- const defaultLayoutPluginInstance = defaultLayoutPlugin();
-  // for onchange event
- const [pdfFile, setPdfFile]=useState(null);
- const [pdfFileError, setPdfFileError]=useState('');
  
- // for submit event
- const [viewPdf, setViewPdf]=useState(null);
+ const listItem = () => {
+   storage.ref().child('curriculum/').listAll()
+     .then(res => {
+       res.items.forEach((item) => {
+         setData(arr => [...arr, item.name]);
+         storage.ref("curriculum").child(item.name).getDownloadURL()
+           .then((url) => {
+             setUrl(url);
+           })
  
- // onchange event
- const fileType=['application/pdf'];
- const handlePdfFileChange=(e)=>{
-   let selectedFile=e.target.files[0];
-   if(selectedFile){
-     if(selectedFile&&fileType.includes(selectedFile.type)){
-       storage.ref(`/files/${pdfFile.}`).put(pdfFile);
-       let reader = new FileReader();
-           reader.readAsDataURL(selectedFile);
-           reader.onloadend = (e) =>{
-             setPdfFile(e.target.result);
-             setPdfFileError('');
-           }
-     }
-     else{
-       setPdfFile(null);
-       setPdfFileError('Please select valid pdf file');
-     }
-   }
-   else{
-     console.log('select your file');
-   }
+       })
+     })
+     .catch(err => {
+       alert(err.message);
+     });
  }
  
- // form submit
- const handlePdfFileSubmit=(e)=>{
-   e.preventDefault();
-   if(pdfFile!==null){
-     setViewPdf(pdfFile);
+ const upload = () => {  
+   if (file ==  null)
+   {
+     return;
    }
-   else{
-     setViewPdf(null);
-   }
- }
- return (
- 
-     <div className="justify-content-md-center" display="flex" text-align="center" align-items="center" text-decoration="none" padding="0">
-     <br></br>
   
-     <form className='form-group' onSubmit={handlePdfFileSubmit}>
-       <input type="file" className='form-control'
-         required onChange={handlePdfFileChange}
-       />
-       {pdfFileError&&<div className='error-msg'>{pdfFileError}</div>}
-       <br></br>
-       <br></br>
-       <button type="submit" className='btn btn-success btn-lg'>
-         UPLOAD
-       </button>
-     </form>
-     <br></br>
-     <h4>View PDF</h4>
-     <div className='pdf-container'>
-       {/* show pdf conditionally (if we have one)  }/*
-       {viewPdf&&<><Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-         <Viewer fileUrl={viewPdf}
-           plugins={[defaultLayoutPluginInstance]} />
-     </Worker></>}
+   // sending file to storage
+   if (file.size > 160000)
+   {
+     alert("File too big! Keep it under 160 MB");
+     return;
+   }
+   storage.ref(`/curriculum/${file.name}`).put(file);
+   alert("Successfully uploaded! Plsease refreash the page");
+ }
  
-     {/* if we dont have pdf or viewPdf state is null } /*
-     {!viewPdf&&<>No pdf file selected</>}
-     </div>
-     </div>
  
- ) */
+ return(
+  <div className="Curriculum" style={{ marginTop: 200 }}>
+   <center>
+     <input type="file" accept=".png, .jpg, .jpeg, .pdf, .docx" onChange={(e) => { setFile(e.target.files[0]) }} />
+     <button class="... md:bg-gray-300 md:hover:bg-gray-500 ..." style={{padding: 3}} onClick={upload}> Upload </button>
+     <br />
+     <p> </p>
+     <button class="... md:bg-gray-300 md:hover:bg-gray-500 ..." style={{padding: 2}} onClick={listItem}> List Item </button>
+       <br /><br />
+  </center>
+  <br /><br />
+       {
+           data.map((val) => ( //val is the name of the file we get from list all, url we get it from geturldownload
+             <ListGroup as="ol" numbered>
+
+               <ListGroup.Item as="li"> <a href={Url}>{val}</a> 
+               <div class="overflow-auto " style={{textAlign:"center"}}>
+               <button class="... md:bg-gray-300 md:hover:bg-gray-500 ..." style={{padding: 1}} onClick={()=>storage.refFromURL(Url).delete().then(()=>alert("Successfully deleted, Please refreash the page!"))}>Delete</button>    
+                </div>
+               </ListGroup.Item>
+             </ListGroup>
+           ))
+       }  
+  </div>
+ //style={{padding: 1, textAlign: 'center'style={styles.space}}
+ );
 }
+
  
 export default Curriculum;
--- src/screens/lessons.js
+++ src/screens/lessons.js
@@ -1,19 +1,76 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import storage from './firebase';
+import ListGroup from 'react-bootstrap/ListGroup'
+import '../index.js'
 const Lessons = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Top',
-        alignItems: 'center',
-        height: '100vh'
-      }}
-    >
-    <h1>Lessons Plans: </h1>
-    </div>
+ // define new handler, we need to handel event (e) and the refrence to the file
+ const [Url, setUrl] = useState('');
+ const [file, setFile] = useState('');
+ const [data, setData] = useState([]);
+ 
+ 
 
-  );
-};
+
+ const listItem = () => {
+   storage.ref().child('lessons/').listAll()
+     .then(res => {
+       res.items.forEach((item) => {
+         setData(arr => [...arr, item.name]);
+         storage.ref("lessons").child(item.name).getDownloadURL()
+           .then((url) => {
+             setUrl(url);
+           })
+ 
+       })
+     })
+     .catch(err => {
+       alert(err.message);
+     });
+ }
+ 
+ const upload = () => {  
+   if (file ==  null)
+   {
+     return;
+   }
   
+   // sending file to storage
+   if (file.size > 160000)
+   {
+     alert("File too big! Keep it under 160 MB");
+     return;
+   }
+   storage.ref(`/lessons/${file.name}`).put(file);
+   alert("Successfully uploaded! Plsease refreash the page");
+ }
+ 
+ 
+ return(
+  <div className="Lessons" style={{ marginTop: 250 }}>
+   <center>
+     <input type="file" accept=".png, .jpg, .jpeg, .pdf, .docx"  onChange={(e) => { setFile(e.target.files[0]) }} />
+     <button class="... md:bg-gray-300 md:hover:bg-gray-500 ..." onClick={upload}> Upload </button>
+     <br />
+     <p> </p>
+     <button class=" bg-gray-300 md:hover:bg-gray-500" onClick={listItem}> List Item </button>
+       <br /><br />
+       </center>
+       {
+           data.map((val) => ( //val is the name of the file we get from list all, url we get it from geturldownload
+             <ListGroup as="ol" numbered>
+               <ListGroup.Item as="li"> <a href={Url}>{val}</a> 
+               <div class="overflow-auto " style={{textAlign:"center"}}>
+                  <button class="bg-gray-300 md:hover:bg-gray-500" onClick={()=>storage.refFromURL(Url).delete().then(()=>alert("Successfully deleted, Please refreash the page!"))}>Delete</button>  
+                </div>
+               </ListGroup.Item>
+  
+             </ListGroup>
+           ))
+       }
+  </div>
+ 
+ );
+}
+
 export default Lessons;
